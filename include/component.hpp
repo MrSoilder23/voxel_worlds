17d4942fb1f66e5d810be7377ba6cddbcdfa8795
MrSoilder <filip.makos49@gmail.com>
2024-12-15 20:37:50 +0100

    feat: add component

--- include/component.hpp
+++ include/component.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
 class IComponent {
-    virtual void Update();
+    public:
+        virtual void Update();
 };
---
+++ include/model_component.hpp
@@ -0,0 +1,11 @@
+#pragma once
+// Own libraries
+#include "component.hpp"
+
+class ModelComponent : public IComponent {
+    public: 
+        void Update() override;
+
+    private:
+        
+};
